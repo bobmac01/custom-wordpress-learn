7c7b5af9f14e47b0e543a90ff4bdef4c167910b0
Bob McAteer <bob@robertmcateer.com>
2020-05-07 15:59:45 +0100

    Completed Section 11: Cleaning up the code

--- functions.php
+++ functions.php
@@ -37,17 +37,49 @@ function university_adjust_queries($query) {
         $query->set('orderby','meta_value_num');
         $query->set('order', 'ASC');
         $query->set('meta_query', array(
-                    array(
-                     'key' => 'event_date',
-                        'compare' => '>=',
-                        'value' => $today,
-                        'type' => 'numeric'
-                    )
-                ));
+            array(
+             'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+        ));
     }
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+function page_banner($args = null) {
+
+    if(!$args['title']) {
+        $args['title'] = get_the_title();
+    }
+    if(!$args['subtitle']) {
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+    if(!$args['bgImg']) {
+        if(get_field('page_banner_background_image')) {
+            $args['bgImg'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+        } else {
+            $args['bgImg'] = get_theme_file_uri('/images/ocean.jpg');
+        }       
+    }
+    
+    ?>
+
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php 
+      echo $args['bgImg']
+       ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+          <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+          <div class="page-banner__intro">
+            <p><?php echo $args['subtitle'] ?></p>
+          </div>
+        </div>  
+    </div>
+<?php
+}
 
 
 
